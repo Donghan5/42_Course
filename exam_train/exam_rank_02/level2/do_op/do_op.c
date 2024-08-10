399dbda54c1b056eb56ae8aa3bd1e2f81fb3a5bd
Donghan01 <cubkedh@naver.com>
2024-08-11 00:43:37 +0200

    have to study

--- exam_train/exam_rank_02/level2/do_op/do_op.c
+++ exam_train/exam_rank_02/level2/do_op/do_op.c
@@ -1,3 +1,31 @@
 /* During the exam sesson no norm rules */
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+void	do_op(char *str1, char operation, char *str2)
+{
+	int op1 = atoi(str1);
+	int op2 = atoi(str2);
+	int	res = 0;
+	if (operation == '+')
+		res = op1 + op2;
+	else if (operation == '-')
+		res = op1 - op2;
+	else if (operation == '*')
+		res = op1 * op2;
+	else if (operation == '/')
+		res = op1 / op2;
+	printf("%d", res);
+}
+
+int main(int ac, char **av)
+{
+	if (ac == 4)
+	{
+		if (av[2][1] == '\0')
+			do_op(av[1], av[2][0], av[3]);
+	}
+	printf("\n");
+	return (0);
+}
