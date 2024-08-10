cf190fe5745e4f32f9895a388c217931f4837d22
Donghan Kim <donghank@paul-f4Ar6s7.clusters.42paris.fr>
2024-08-10 17:08:37 +0200

    del dsstore

---
+++ exam_train/exam_rank_02/level2/camel_to_snake/camel_to_snake.c
@@ -0,0 +1,22 @@
+/* During the exam there is no norm rules */
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		int i = 0;
+		while (av[1][i] != '\0')
+		{
+			if (av[1][i] >= 65 && av[1][i] <= 90)
+			{
+				av[1][i] += 32;
+				write(1, "_", 1);
+			}
+			write(1, &av[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
---
+++ exam_train/exam_rank_02/level2/do_op/do_op.c
@@ -0,0 +1,3 @@
+/* During the exam sesson no norm rules */
+#include <stdio.h>
+#include <unistd.h>
