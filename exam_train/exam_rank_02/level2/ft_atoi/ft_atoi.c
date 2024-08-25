63d4e72b4659600f4166d1867bb1e25a1a249b0c
Donghan Kim <donghank@made-f0Br8s19.clusters.42paris.fr>
2024-08-25 16:05:45 +0200

    level 2 3 4

--- exam_train/exam_rank_02/level2/ft_atoi/ft_atoi.c
+++ exam_train/exam_rank_02/level2/ft_atoi/ft_atoi.c
@@ -6,7 +6,7 @@ int	ft_atoi(char *str)
 	int	sign = 1;
 	int	res = 0;
 
-	while (str[idx] == ' ' || (str[idx] == '\t' && str[idx] == '\r'))
+	while (str[idx] == ' ' || (str[idx] >= 9 && str[idx] <= 13))
 		idx++;
 	if (str[idx] == '+' || str[idx] == '-')
 	{
@@ -22,3 +22,27 @@ int	ft_atoi(char *str)
 	}
 	return (res * sign);
 }
+
+/*
+another sol
+int ft_atoi(const char *str)
+{
+	int sign = 1;
+	int res = 0;
+
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - '0');
+		str++;
+	}
+	return (sign * res);
+}
+*/
