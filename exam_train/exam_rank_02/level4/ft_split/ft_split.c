b6cee48576e17dfc88d7ea24a8359e8ec0bc8530
Donghan Kim <donghank@made-f0Ar11s1.clusters.42paris.fr>
2024-08-28 17:32:31 +0200

    split itoa strcap rstrcap all level 3 and 4 some 2

--- exam_train/exam_rank_02/level4/ft_split/ft_split.c
+++ exam_train/exam_rank_02/level4/ft_split/ft_split.c
@@ -1,51 +1,60 @@
 /* Exam no norm */
 #include <stdlib.h>
 
-char	*ft_strncpy(char *s1, char *s2, int n)
+// to copy the arrays ot out(double char pointer)
+char *ft_strncpy(char *s1, char *s2, int n)
 {
 	int i = -1;
-	while (++i < n && s2[i])
+	while (++i < n && s2[i] != '\0')
 		s1[i] = s2[i];
 	s1[i] = '\0';
 	return (s1);
 }
 
-char	**ft_split(char *str)
+char	*ft_split(char *str)
 {
 	int i = 0;
-	int j = 0; // index of the start...?
-	int k = 0; // index of the splitted output (double pointer)
-	int word_count = 0;
+	int start = 0; // index of the start of the word
+	int k = 0; // index of the double array
+	int wc = 0; // word count
+
 	while (str[i])
 	{
-		// skip space, newline and tab before the first word
-		while (str[i] && (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'))
+		// pass the space or tab or newline character
+		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
 			i++;
-		while (str[i]) // count the word(character)
-			word_count++;
-		// pass spaces newline and tab after the word
-		while (str[i] && str[i] != ' ' || str[i] != '\n' || str[i] != '\t')
+		// if the str[i] increment the wc(word count)
+		if (str[i]) // using the if condition not while loop
+			wc++;
+		// when str[i] isn't space tab or newline character, increment the index
+		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
 			i++;
 	}
-	// declare the double pointer of the char to stock each words.
-	char **out = (char **)malloc(sizeof(char *) * (word_count + 1));
+	// declare the double pointer of char to stock each word
+	char **out = (char **)malloc(sizeof(char *) * (wc + 1));
+	// initialize i = 0
 	i = 0;
 	while (str[i])
 	{
-		// skip(pass) the space, newline and tab before the word
-		while (str[i] && (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'))
+		// pass the space tab or newline
+		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
 			i++;
-		// setting the start point
-		j = i;
-		// pass the space, newline and tab after the word
-		while (str[i] && (str[i] != ' ' || str[i] != '\n' || str[i] != '\t'))
+		// setting start
+		start = i;
+		// during the word (I assume)
+		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
 			i++;
-		while (i > j)
+		// if i is bigger than start
+		if (i > start)
 		{
-			out[k] = (char *)malloc(sizeof(char) * (i - j + 1));
-			ft_strncpy(out[k++], &str[j], i - j);
+			// declare element of the out (in sigle array)
+			// beware of the length --> (i - start) + 1
+			out[k] = (char *)malloc(sizeof(char) * ((i - start) + 1));
+			// copy each word out[k++], &str[start], and length(i - start)
+			ft_strncpy(out[k++], &str[start], i - start);
 		}
-		out[k] = NULL;
 	}
+	// don't forget to set the last element to null
+	out[k] = NULL;
 	return (out);
 }
