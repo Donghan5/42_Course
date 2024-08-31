c0f744ca0b35034c21b42673a30efceab24de2aa
Donghan01 <cubkedh@naver.com>
2024-08-31 09:26:27 +0200

    Let's start to implement philosopher

---
+++ circle3/philosophers/philo/includes/philo.h
@@ -0,0 +1,15 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+/*==================== header =====================*/
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <memory.h>
+# include <sys/time.h>
+
+/*==================== utils.c =====================*/
+int		ft_atoi(char *str);
+
+#endif
---
+++ circle3/philosophers/philo/srcs/main.c
@@ -0,0 +1,9 @@
+#include "philo.h"
+
+int	main(int ac, char **av)
+{
+	if (ac != 5 && ac != 6)
+		// print error msg
+	// if initialize is fail -> print error msg
+	// if run thread of the philo is fail -> print error msg
+}
