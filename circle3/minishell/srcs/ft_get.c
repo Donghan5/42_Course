fc2256300717d02f4791007061fcafbd6854935b
Donghan Kim <donghank@paul-f4Ar2s2.clusters.42paris.fr>
2024-10-07 14:24:51 +0200

    make original

--- circle3/minishell/srcs/ft_get.c
+++ circle3/minishell/srcs/ft_get.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_get.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: donghan <donghan@student.42.fr>            +#+  +:+       +#+        */
+/*   By: donghank <donghank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 13:24:38 by donghank          #+#    #+#             */
-/*   Updated: 2024/10/04 10:56:38 by donghan          ###   ########.fr       */
+/*   Updated: 2024/10/07 14:20:12 by donghank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,12 +64,12 @@ char	*replace_home_tilde(char *cwd)
 	return (cwd);
 }
 
-// to get hostname in the /usr/hostname
+// to get the hostname
 char	*get_hostname(void)
 {
 	int		fd;
 	char	*hostname;
-	char	*current;
+	char	*newline_pos;
 
 	fd = open("/etc/hostname", O_RDONLY);
 	if (fd < 0)
@@ -78,16 +78,9 @@ char	*get_hostname(void)
 	close(fd);
 	if (hostname)
 	{
-		current = hostname;
-		while (*current)
-		{
-			if (*current == '\n' || *current == '.')
-			{
-				*current = '\0';
-				break ;
-			}
-			current++;
-		}
+		newline_pos = ft_strchr(hostname, '.');
+		if (newline_pos)
+			*newline_pos = '\0';
 	}
 	return (hostname);
 }
