40e734b155d1cb8380e4ca917c2cce90c8ba98ff
Donghan Kim <donghank@paul-f4Br9s3.clusters.42paris.fr>
2024-10-08 14:02:15 +0200

    make original

--- circle3/minishell/main.c
+++ circle3/minishell/main.c
@@ -6,7 +6,7 @@
 /*   By: donghank <donghank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 00:58:35 by pzinurov          #+#    #+#             */
-/*   Updated: 2024/10/08 12:34:30 by donghank         ###   ########.fr       */
+/*   Updated: 2024/10/08 13:59:44 by donghank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,7 +89,7 @@ int	main(int argc, char **argv, char **envp)
 	(void)argv;
 	parse_env(&env, envp);
 	increment_shell_level(&env);
-	header();
+	// header();
 	using_history();
 	set_signal();
 	while (1)
