5c61c26f9dad8516f23bff79951ce4a14089549d
donghank <donghank@student.42.fr>
2024-10-24 17:22:08 +0200

    sdsd

--- circle3/minishell/srcs/heredoc.c
+++ circle3/minishell/srcs/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: pzinurov <pzinurov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:33:12 by pzinurov          #+#    #+#             */
-/*   Updated: 2024/10/18 20:45:20 by pzinurov         ###   ########.fr       */
+/*   Updated: 2024/10/24 16:34:00 by pzinurov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,46 +40,41 @@ int	ft_heredoc(char *stop_word, int fd, t_env *env)
 	{
 		g_signal_received = 0;
 		line = readline("> ");
-		if (!line || g_signal_received)
+		if (!line || (g_signal_received == SIGINT))
 			break ;
 		if (!ft_strncmp(stop_word, line, ft_strlen(stop_word) + 1))
 			break ;
 		expanded_line = expander_heredoc(line, env);
 		if (!expanded_line)
-			return (free(line), 0);
+			return (free(line), rl_event_hook = NULL, rl_done = 0, 0);
 		ft_putendl_fd(expanded_line, fd);
 		free(line);
 		free(expanded_line);
 	}
 	if (line)
 		free(line);
-	if (!line && !g_signal_received)
+	if (!line && (g_signal_received != SIGINT))
 		printf("%s (wanted `%s')\n", HDOC_ERR, stop_word);
 	if (g_signal_received == SIGINT)
-		return (0);
+		return (rl_event_hook = NULL, rl_done = 0, 0);
 	return (rl_event_hook = NULL, rl_done = 0, 1);
 }
 
 int	setup_heredoc(t_glob_pipe *current, t_glob_pipe *next, int *fd, t_env *env)
 {
-	int			heredoc_status;
-	t_sigaction	sa;
+	int	heredoc_status;
 
 	*fd = open("/tmp/sh-thd-86500896238475834",
 			O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (*fd < 0)
 		return (print_file_err("/tmp/sh-thd-86500896238475834"));
-	sa.sa_handler = heredoc_sigint_handler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sigaction(SIGINT, &sa, NULL);
+	signal(SIGINT, heredoc_sigint_handler);
 	rl_event_hook = event_hook;
 	heredoc_status = ft_heredoc(next->name, *fd, env);
 	smart_close(*fd);
 	set_signal();
-	g_signal_received = 0;
-	if (!heredoc_status)
-		return (unlink("/tmp/sh-thd-86500896238475834"), env->sts = 130, 0);
+	if (g_signal_received == SIGINT)
+		return (unlink("/tmp/sh-thd-86500896238475834"), 0);
 	*fd = open("/tmp/sh-thd-86500896238475834", O_RDONLY);
 	if ((*fd) < 0)
 		return (unlink("/tmp/sh-thd-86500896238475834"), env->sts = 1,
