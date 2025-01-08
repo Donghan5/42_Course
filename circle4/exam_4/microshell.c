d4b62334ea6f0ce30ecb5f9b332efe8642cc3c4b
Donghan Kim <donghank@paul-f3Br3s5.clusters.42paris.fr>
2025-01-08 15:10:57 +0100

    added comment to fix added

--- circle4/exam_4/microshell.c
+++ circle4/exam_4/microshell.c
@@ -9,12 +9,12 @@ void err(char *str) {
 	}
 }
 
-// it doesn't return the value
+
 int cd(char **argv, int i) {
-	if (i != 2) {
+	if (i != 2) { // have to return 1
 		return (err("error: cd: bad arguments\n"), 1);
 	}
-	if (chdir(argv[1]) < 0) {
+	if (chdir(argv[1]) < 0) { // if chdir is negative value --> error
 		err("error: cd: cannot change directory to ");
 		err(argv[1]);
 		err("\n");
@@ -24,6 +24,7 @@ int cd(char **argv, int i) {
 }
 
 // this part set param
+// has_pipe == boolean fd and end
 void set_pipe(int has_pipe, int *fd, int end) {
 	if (has_pipe && (dup2(fd[end], end) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1)) {
 		err("error: fatal\n");
@@ -33,12 +34,14 @@ void set_pipe(int has_pipe, int *fd, int end) {
 
 // this part have to study
 // check return value
+// cd verify have to be with pointer
 int exec(char **argv, int i, char **envp) {
 	int has_pipe, fd[2], pid, status;
 
 	has_pipe = argv[i] && (!strcmp(argv[i], "|"));
 	// forget cd check
-	if (!has_pipe && !strcmp(*argv, "cd")) {
+	// do not have pipe and !strcmp(*argv, "cd")
+	if (!has_pipe && !strcmp(*argv, "cd")) { // check the conditions
 		return (cd(argv, i));
 	}
 	if (has_pipe && pipe(fd) == -1) {
@@ -50,9 +53,9 @@ int exec(char **argv, int i, char **envp) {
 		exit(1);
 	}
 	if (!pid) {
-		argv[i] = 0;
-		set_pipe(has_pipe, fd, 1);
-		if (!strcmp(*argv, "cd")) {
+		argv[i] = 0; // terminate the session
+		set_pipe(has_pipe, fd, 1); // out pipe
+		if (!strcmp(*argv, "cd")) { // cd check
 			exit(cd(argv, i));
 		}
 		execve(*argv, argv, envp);
@@ -62,7 +65,7 @@ int exec(char **argv, int i, char **envp) {
 		exit(1);
 	}
 	waitpid(pid, &status, 0);
-	set_pipe(has_pipe, fd, 0);
+	set_pipe(has_pipe, fd, 0); // in pipe
 	return (WIFEXITED(status) && WEXITSTATUS(status));
 }
 
